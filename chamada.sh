93886484cb8d1b6f3323188435fb331fc9a175a2
Jurandy Soares <jurandysoares@users.noreply.github.com>
2015-06-03 17:58:50 -0300

    Criação de script para agilizar a chamada

---
+++ chamada.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# chamada.c
+# This file is part of chamada.sh
+#
+# Copyright (C) 2015 - Jurandy Soares
+#
+# chamada.sh is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# chamada.sh is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with chamada.sh. If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Script que lista o nome completo de quem iniciou sessão no horário da aula
+
+# A-FAZER: 
+#   * extrair ano, mês, dia do ambiente (amd=$(date +'%Y%m%d'))
+#   * deduzir hora e minuto de início da aula (md=$(date +'%H%M'))
+
+quem_acessou=$(last -t 20150603163000 |  \
+               awk '{print $1}' | \
+               sort -u | \
+               egrep -v '^(jurandy|reboot|wtmp|^$)'
+               )
+               
+for u in $quem_acessou; 
+  do 
+    egrep "^${u}:" /etc/passwd | \
+    awk -F: '{print $5}'; 
+  done | sort
